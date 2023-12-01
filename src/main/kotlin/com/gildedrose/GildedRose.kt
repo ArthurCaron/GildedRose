24bcb0568bfe280847eb1c573237b635a6686e6b
Arthur Caron <arthur.caron@malt.com>
2023-12-01 19:00:06 +0100

    Make GenericItem the default one

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -12,8 +12,8 @@ class GildedRose(var items: Array<Item>) {
                 items[i] = when (name) {
                     AGED_BRIE -> AgedBrie(name, sellIn, quality).oneDayPasses()
                     BACKSTAGE -> Backstage(name, sellIn, quality).oneDayPasses()
-                    SULFURAS -> GeneralItem(name, sellIn, quality).oneDayPasses()
-                    else -> Other(name, sellIn, quality).oneDayPasses()
+                    SULFURAS -> Sulfuras(name, sellIn, quality).oneDayPasses()
+                    else -> GeneralItem(name, sellIn, quality).oneDayPasses()
                 }
             }
         }
@@ -22,42 +22,48 @@ class GildedRose(var items: Array<Item>) {
     open class GeneralItem(val name: String, var sellIn: Int, _quality: Int) {
         var quality = _quality
             set(newQuality) {
-                field = if (newQuality > 50) 50 else newQuality
+                field = if (newQuality > 50)
+                    50
+                else if (newQuality < 0)
+                    0
+                else
+                    newQuality
             }
 
-        open fun oneDayPasses(): Item = Item(name, sellIn, quality)
+        open fun updateSellIn() {
+            sellIn--
+        }
+
+        open fun updateQuality() {
+            quality -= if (sellIn < 0) 2 else 1
+        }
+
+        fun oneDayPasses(): Item {
+            updateSellIn()
+            updateQuality()
+            return Item(name, sellIn, quality)
+        }
     }
 
     class AgedBrie(name: String, sellIn: Int, quality: Int) : GeneralItem(name, sellIn, quality) {
-        override fun oneDayPasses(): Item {
-            sellIn--
-            quality += if (sellIn < 0) 2 else 1
-            return Item(name, sellIn, quality)
+        override fun updateQuality() {
+            quality++
+            if (sellIn < 0) quality++
         }
     }
 
     class Backstage(name: String, sellIn: Int, quality: Int) : GeneralItem(name, sellIn, quality) {
-        override fun oneDayPasses(): Item {
-            sellIn--
-            quality = if (sellIn >= 10) {
-                quality + 1
-            } else if (sellIn >= 5) {
-                quality + 2
-            } else if (sellIn >= 0) {
-                quality + 3
-            } else {
-                0
-            }
-            return Item(name, sellIn, quality)
+        override fun updateQuality() {
+            quality++
+            if (sellIn < 10) quality++
+            if (sellIn < 5) quality++
+            if (sellIn < 0) quality = 0
         }
     }
 
-    class Other(name: String, sellIn: Int, quality: Int) : GeneralItem(name, sellIn, quality) {
-        override fun oneDayPasses(): Item {
-            sellIn--
-            quality -= if (sellIn < 0 && quality > 1) 2 else if (quality > 0) 1 else 0
-            return Item(name, sellIn, quality)
-        }
+    class Sulfuras(name: String, sellIn: Int, quality: Int) : GeneralItem(name, sellIn, quality) {
+        override fun updateSellIn() {}
+        override fun updateQuality() {}
     }
 }
 
